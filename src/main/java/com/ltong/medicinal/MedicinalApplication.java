2f2b3c23590396c4f904c646e122c6832c2136d1
LTong <liutongspark@163.com>
2020-08-28 17:13:21 +0800

    修改

--- src/main/java/com/ltong/medicinal/MedicinalApplication.java
+++ src/main/java/com/ltong/medicinal/MedicinalApplication.java
@@ -2,13 +2,7 @@ package com.ltong.medicinal;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.web.servlet.MultipartConfigFactory;
-import org.springframework.boot.web.servlet.ServletComponentScan;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.util.unit.DataSize;
-
-import javax.servlet.MultipartConfigElement;
 
 @Configuration
 @SpringBootApplication
